7f5d86b2544422d57d77ef28c50798d92a7cbae5
EdgarAlvarado24 <edgar.alvarado.16@gmail.com>
2026-02-13 12:57:24 -0400

    added logos

--- src/pages/OrdersPage.jsx
+++ src/pages/OrdersPage.jsx
@@ -1,4 +1,5 @@
 import { Header } from '../components/Header';
+import { Link } from 'react-router';
 import './OrdersPage.css';
 
 export function OrdersPage() {
@@ -6,7 +7,7 @@ export function OrdersPage() {
     <>
       <link rel="icon" type="image/svg+xml" href="/orders-favicon.png" />
       <title>Orders</title>
-      
+
       <Header/>
 
       <div className="orders-page">
@@ -55,11 +56,11 @@ export function OrdersPage() {
               </div>
 
               <div className="product-actions">
-                <a href="/tracking">
+                <Link href="/tracking">
                   <button className="track-package-button button-secondary">
                     Track package
                   </button>
-                </a>
+                </Link>
               </div>
 
               <div className="product-image-container">
@@ -83,11 +84,11 @@ export function OrdersPage() {
               </div>
 
               <div className="product-actions">
-                <a href="/tracking">
+                <Link href="/tracking">
                   <button className="track-package-button button-secondary">
                     Track package
                   </button>
-                </a>
+                </Link>
               </div>
             </div>
           </div>
@@ -134,11 +135,11 @@ export function OrdersPage() {
               </div>
 
               <div className="product-actions">
-                <a href="/tracking">
+                <Link href="/tracking">
                   <button className="track-package-button button-secondary">
                     Track package
                   </button>
-                </a>
+                </Link>
               </div>
             </div>
           </div>
